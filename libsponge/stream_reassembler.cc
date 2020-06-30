87bef8cac3ca79a97b22bea2ceada113868f3501
Colin <qingyu.wang@aliyun.com>
2020-06-30 21:24:35 +0800

    feat(lab1): finish lab1
    passing all test cases

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,5 +1,7 @@
 #include "stream_reassembler.hh"
 
+#include <iostream>
+
 // Dummy implementation of a stream reassembler.
 
 // For Lab 1, please replace with a real implementation that passes the
@@ -12,15 +14,37 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _output(capacity), _capacity(capacity), _begin(0), _end(0), _buf(capacity, 0), _writen(capacity, false) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (eof) {
+        _eof = true;
+    }
+
+    std::copy(data.begin(), data.end(), _buf.begin() + index);
+    std::fill_n(_writen.begin() + index, data.size(), true);
+    _end = std::max(_end, index + data.size());
+
+    auto it = _writen.begin() + _begin;
+    size_t count = 0;
+    while (*it) {
+        *it = false;
+        it++;
+        count++;
+    }
+
+    _output.write(_buf.substr(_begin, count));
+    _begin += count;
+
+    if (_eof && empty()) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return std::accumulate(_writen.begin(), _writen.end(), 0); }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _end == _begin; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,7 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -14,6 +15,12 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+
+    size_t _begin;
+    size_t _end;
+    bool _eof = false;
+    std::string _buf;
+    std::vector<bool> _writen;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -31,7 +38,7 @@ class StreamReassembler {
     //! \param data the string being added
     //! \param index the index of the first byte in `data`
     //! \param eof whether or not this segment ends with the end of the stream
-    void push_substring(const std::string &data, const uint64_t index, const bool eof);
+    void push_substring(const std::string &data, const size_t index, const bool eof);
 
     //! \name Access the reassembled byte stream
     //!@{
